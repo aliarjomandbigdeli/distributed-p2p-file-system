ebf7351b07adc5533a67989d3a0cdecd0ab6f30f
aliarjomandbigdeli <aliabigdeli@gmail.com>
2018-12-19 14:45:29 +0330

    initiate udp connection

---
+++ src/Client.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.net.*;
+
+public class Client {
+    private DatagramSocket socket;
+    private InetAddress address;
+
+    private byte[] buf;
+
+    public Client() {
+        try {
+            socket = new DatagramSocket();
+            address = InetAddress.getByName("localhost");
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String sendEcho(String msg) {
+        buf = msg.getBytes();
+        DatagramPacket packet
+                = new DatagramPacket(buf, buf.length, address, 4445);
+        try {
+            socket.send(packet);
+            packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String received = new String(
+                packet.getData(), 0, packet.getLength());
+        return received;
+    }
+
+    public void close() {
+        socket.close();
+    }
+}
